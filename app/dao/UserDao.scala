05081078ccb30c04d00ca537541043cf7e9b036d
Alexander Worton <kesomir@btinternet.com>
2017-06-30 21:52:49 +0100

    Added scalatyle, licence and cleaned code up to better meet scalastyle linting

--- app/dao/UserDao.scala
+++ app/dao/UserDao.scala
@@ -1,3 +1,19 @@
+// Copyright (C) 2017 Alexander Worton.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package dao
 
 import java.sql.{Date, Timestamp}
@@ -8,12 +24,10 @@ import slick.jdbc.JdbcProfile
 import models.User
 import play.api.db.slick.DatabaseConfigProvider
 import play.api.db.slick.HasDatabaseConfigProvider
+import slick.lifted.ProvenShape
 
 import scala.concurrent.{ExecutionContext, Future}
 
-/**
-  * @author Alexander Worton.
-  */
 class UserDao @Inject()
     (protected val dbConfigProvider: DatabaseConfigProvider)
     (implicit executionContext: ExecutionContext)
@@ -40,22 +54,22 @@ class UserDao @Inject()
       d => d.toLocalDateTime
     )
 
-    def id = column[Int]("Id", O.PrimaryKey)
-    def userName = column[String]("UserName")
-    def firstName = column[String]("FirstName")
-    def surName = column[String]("UserName")
-    def email = column[String]("UserName")
-    def UserName = column[String]("UserName")
-    def isEmailVerified = column[Boolean]("IsEmailVerified")
-    def authHash = column[String]("AuthHash")
-    def authResetCode = column[Option[String]]("AuthResetCode")
-    def authResetExpiry = column[Option[LocalDate]]("AuthResetExpiry")
-    def authToken = column[Option[String]]("AuthToken")
-    def authExpire = column[Option[LocalDateTime]]("AuthExpire")
-    def isEducator = column[Boolean]("IsEducator")
-    def isAdministrator = column[Boolean]("IsAdministrator")
+    def id: Rep[Int] = column[Int]("Id", O.PrimaryKey)
+    def userName: Rep[String] = column[String]("UserName")
+    def firstName: Rep[String] = column[String]("FirstName")
+    def surName: Rep[String] = column[String]("UserName")
+    def email: Rep[String] = column[String]("UserName")
+    def UserName: Rep[String] = column[String]("UserName")
+    def isEmailVerified: Rep[Boolean] = column[Boolean]("IsEmailVerified")
+    def authHash: Rep[String] = column[String]("AuthHash")
+    def authResetCode: Rep[Option[String]] = column[Option[String]]("AuthResetCode")
+    def authResetExpiry: Rep[Option[LocalDate]] = column[Option[LocalDate]]("AuthResetExpiry")
+    def authToken: Rep[Option[String]] = column[Option[String]]("AuthToken")
+    def authExpire: Rep[Option[LocalDateTime]] = column[Option[LocalDateTime]]("AuthExpire")
+    def isEducator: Rep[Boolean] = column[Boolean]("IsEducator")
+    def isAdministrator: Rep[Boolean] = column[Boolean]("IsAdministrator")
 
-    def * = (
+    def * : ProvenShape[User] = (
       id,
       userName,
       firstName,
--- app/dto/UserDto.scala
+++ app/dto/UserDto.scala
@@ -1,12 +1,23 @@
+// Copyright (C) 2017 Alexander Worton.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package dto
 
-import java.time.LocalDate
-
 import models.User
-import play.api.libs.functional.syntax.unlift
 import play.api.libs.json.{Format, Json, __}
-import play.api.libs.json._
-import play.api.libs.functional.syntax._
 
 /**
   * @author Alexander Worton.
@@ -25,7 +36,7 @@ case class UserDto(
 
 object UserDto {
 
-  def apply(user: User) = {
+  def apply(user: User): UserDto = {
     new UserDto(
       user.id,
       user.userName,
