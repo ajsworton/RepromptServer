7288c0648df571b20d13ff2da39dbb6598cc61a4
Alexander Worton <kesomir@btinternet.com>
2017-07-11 23:02:35 +0100

    Changed mysql restart commands

--- scripts/mysql-reset-root-password.sh
+++ scripts/mysql-reset-root-password.sh
@@ -8,5 +8,7 @@ sleep 4
 sudo mysql -e "use mysql; update user set authentication_string=PASSWORD('') where User='root'; update user set plugin='mysql_native_password';FLUSH PRIVILEGES;"
 sudo kill -9 `sudo cat /var/lib/mysql/mysqld_safe.pid`
 sudo kill -9 `sudo cat /var/run/mysqld/mysqld.pid`
-sudo service mysql restart
+sudo mysqladmin shutdown
 sleep 4
+sudo service mysql restart
+sudo service mysql start
