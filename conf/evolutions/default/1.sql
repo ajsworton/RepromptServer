32f4a597999b99af59d5ecdb06c89b68dbe7e59f
Alexander Worton <kesomir@btinternet.com>
2017-07-18 10:35:45 +0100

    Sorted evolutions issue

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -39,7 +39,27 @@ CREATE TABLE Profiles(
 INSERT INTO Profiles (UserId, ProviderId, ProviderKey, Confirmed, FirstName)
 VALUES (1, 'credentials', 'admin', 1 , 'Admin');
 
+CREATE TABLE `cohorts` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `OwnerId` int(11) DEFAULT NULL,
+  `Name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `ParentId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `OwnerId` (`OwnerId`),
+  KEY `ParentId` (`ParentId`),
+  CONSTRAINT `cohorts_ibfk_1` FOREIGN KEY (`OwnerId`) REFERENCES `users` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `cohorts_ibfk_2` FOREIGN KEY (`ParentId`) REFERENCES `cohorts` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=160 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `cohort_members` (
+  `CohortId` int(11) NOT NULL,
+  `UserId` int(11) NOT NULL,
+  PRIMARY KEY (`CohortId`,`UserId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 # --- !Downs
 
-DROP TABLE Users;
-DROP TABLE Profiles;
+DROP TABLE IF EXISTS `cohort_members`;
+DROP TABLE IF EXISTS `cohorts`;
+DROP TABLE IF EXISTS Profiles;
+DROP TABLE IF EXISTS Users;
